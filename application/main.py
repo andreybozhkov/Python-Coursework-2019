6f963584fc63ee36311751cbc6815b129ef9787f
andreybozhkov <bozhkov.andrey@gmail.com>
2020-01-03 18:23:09 +0200

    Added functionality for reading user input and ending input

--- application/main.py
+++ application/main.py
@@ -1,15 +1,32 @@
-import colli
+from colli import Colli
 
-continueLoop = True
+def getInputFromUser(prop):
+    inputFromUser = input(f'Enter {prop}: ')
+    if inputFromUser == 'end':
+        return 'end'
+    
+    try:
+        value = float(inputFromUser)
+    except ValueError:
+        print('Input is not a number (integer or float)! Please try again.')
+        value = getInputFromUser(prop)
+
+    return value
+
 sequenceNr = 1
+continueInput = True
 
-while True:
-    print(f'Enter dimensions (numbers) for colli nr {sequenceNr}. If you wish to stop at any point, type "end" and hit enter.')
+while continueInput:
+    print(f'Enter dimensions (number integer or float) for colli nr {sequenceNr}. If you wish to stop, type "end" instead and hit enter.')
     colliProperties = ['length', 'width', 'height']
+    tempColli = {
+        'length': 0,
+        'width': 0,
+        'height': 0
+    }
     for prop in colliProperties:
-        inputFromUser = input(f'Enter {prop}: ')
-        try:
-            value = float(inputFromUser)
-        except ValueError:
-            print('Input is not a number (integer or float)! Please try again.')
-    break
+        value = getInputFromUser(prop)
+        if value == 'end':
+            continueInput = False
+            break
+        print(value)
